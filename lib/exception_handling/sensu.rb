789cff55d3229b930a79150f60578f6176b121a6
David Roe <droe@invoca.com>
2015-08-27 13:04:17 -0700

    Actually add sensu handling files

---
+++ lib/exception_handling/sensu.rb
@@ -0,0 +1,27 @@
+require "socket"
+
+module ExceptionHandling
+  class Sensu
+    class << self
+      def generate_event(name, message, level = :warning)
+        status = case level
+                   when :warning
+                     1
+                   when :critical
+                     2
+                   else
+                     raise "Invalid alert level #{level.to_s}"
+                 end
+
+        event = {name: "#{ExceptionHandling.sensu_prefix}#{name}", output: message, status: status}
+
+        send_event(event)
+      end
+
+      def send_event(event)
+        s = TCPSocket.new(ExceptionHandling.sensu_host, ExceptionHandling.sensu_port)
+        s.send(event.to_json)
+      end
+    end
+  end
+end
---
+++ test/unit/exception_handling/sensu_test.rb
@@ -0,0 +1,50 @@
+require File.expand_path('../../../test_helper',  __FILE__)
+
+module ExceptionHandling
+  class SensuTest < ActiveSupport::TestCase
+    context "#generate_event" do
+      should "create an event" do
+        mock(ExceptionHandling::Sensu).send_event({ name: "world_is_ending", output: "stick head between knees and kiss ass goodbye", status: 1 })
+
+        ExceptionHandling::Sensu.generate_event("world_is_ending", "stick head between knees and kiss ass goodbye")
+      end
+
+      should "add the sensu prefix" do
+        ExceptionHandling.sensu_prefix = "cnn_"
+
+        mock(ExceptionHandling::Sensu).send_event({ name: "cnn_world_is_ending", output: "stick head between knees and kiss ass goodbye", status: 1 })
+
+        ExceptionHandling::Sensu.generate_event("world_is_ending", "stick head between knees and kiss ass goodbye")
+      end
+
+      should "allow the level to be set to critical" do
+        mock(ExceptionHandling::Sensu).send_event({ name: "world_is_ending", output: "stick head between knees and kiss ass goodbye", status: 2 })
+
+        ExceptionHandling::Sensu.generate_event("world_is_ending", "stick head between knees and kiss ass goodbye", :critical)
+      end
+
+      should "error if an invalid level is supplied" do
+        dont_allow(ExceptionHandling::Sensu).send_event
+
+        assert_raise RuntimeError do
+          ExceptionHandling::Sensu.generate_event("world_is_ending", "stick head between knees and kiss ass goodbye", :hair_on_fire)
+        end
+      end
+    end
+
+    context "#send_event" do
+      setup do
+        @event = { name: "world_is_ending", output: "stick head between knees and kiss ass goodbye", status: 1 }
+        @socket = SocketStub.new
+      end
+
+      should "send event json to sensu client" do
+        mock(TCPSocket).new("127.0.0.1", 3030) { @socket }
+
+        ExceptionHandling::Sensu.send_event(@event)
+
+        assert_equal @event.to_json, @socket.sent.first
+      end
+    end
+  end
+end
