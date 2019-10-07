4ec8ab55c72a29b12c95ae5a61ff1ee7c0f67232
Donnie Tognazzini <dtognazzini@invoca.com>
2019-10-07 14:01:22 -0700

    handle exceptions in local_variable_filter
    - exceptions from Honeybadger.local_variable_filter can fly out of Honeybadger.notify
    - this can cause send_exception_to_honeybadger to fail.

--- lib/exception_handling/honeybadger_callbacks.rb
+++ lib/exception_handling/honeybadger_callbacks.rb
@@ -11,18 +11,31 @@ module ExceptionHandling
 
       private
 
+      def inspect_object(object, filter_keys)
+        inspection_output = object.inspect
+
+        if contains_filter_key?(filter_keys, inspection_output)
+          filtered_object(object)
+        else
+          inspection_output
+        end
+      rescue => ex
+        details = if object.respond_to?(:to_pk)
+                    " @pk=#{object.to_pk}"
+                  elsif object.respond_to?(:id)
+                    " @id=#{object.id}"
+                  end
+
+        "#<#{object.class.name}#{details} [error '#{ex.class.name} - #{ex.message}' while calling #inspect]>"
+      end
+
       def local_variable_filter(_symbol, object, filter_keys)
         case object
         # Honeybadger will filter these data types for us
         when String, Hash, Array, Set, Numeric, TrueClass, FalseClass, NilClass
           object
         else # handle other Ruby objects, intended for POROs
-          inspection_output = object.inspect
-          if contains_filter_key?(filter_keys, inspection_output)
-            filtered_object(object)
-          else
-            inspection_output
-          end
+          inspect_object(object, filter_keys)
         end
       end
 
