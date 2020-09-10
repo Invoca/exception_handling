2d59bb8908444809c29767a0e683daeecb37833d
Madelyn Romero <m.romero013@gmail.com>
2020-09-10 14:51:42 -0600

    TECH-4812: delegate ExceptionHandling::LoggingMethods to ExceptionHandling

--- lib/exception_handling/logging_methods.rb
+++ lib/exception_handling/logging_methods.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'active_support/concern'
+require 'active_support/core_ext/module/delegation.rb'
 
 module ExceptionHandling
   module LoggingMethods # included on models and controllers
@@ -8,25 +9,11 @@ module ExceptionHandling
 
     protected
 
+    delegate :log_error_rack, :log_warning, :log_info, :log_debug, :escalate_error, :escalate_warning, :ensure_escalation, :alert_warning, to: ExceptionHandling
+
     def log_error(exception_or_string, exception_context = '')
       controller = self if respond_to?(:request) && respond_to?(:session)
       ExceptionHandling.log_error(exception_or_string, exception_context, controller)
-    end
-
-    def log_error_rack(exception_or_string, exception_context = '', rack_filter = '')
-      ExceptionHandling.log_error_rack(exception_or_string, exception_context, rack_filter)
-    end
-
-    def log_warning(message)
-      ExceptionHandling.log_warning(message)
-    end
-
-    def log_info(message)
-      ExceptionHandling.logger.info(message)
-    end
-
-    def log_debug(message)
-      ExceptionHandling.logger.debug(message)
     end
 
     def ensure_safe(exception_context = "")
@@ -34,24 +21,6 @@ module ExceptionHandling
     rescue => ex
       log_error ex, exception_context
       nil
-    end
-
-    def escalate_error(exception_or_string, email_subject)
-      ExceptionHandling.escalate_error(exception_or_string, email_subject)
-    end
-
-    def escalate_warning(message, email_subject)
-      ExceptionHandling.escalate_warning(message, email_subject)
-    end
-
-    def ensure_escalation(*args)
-      ExceptionHandling.ensure_escalation(*args) do
-        yield
-      end
-    end
-
-    def alert_warning(*args)
-      ExceptionHandling.alert_warning(*args)
     end
 
     def ensure_alert(*args)
