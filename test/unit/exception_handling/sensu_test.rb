ebdd65c2ae9aea2bd10626ef5230011f4aba43ea
Yinon Rousso <yrousso@invoca.com>
2020-09-21 11:18:48 -0600

    TECH-4826 change sensu_test.rb to use rspec

--- test/unit/exception_handling/sensu_test.rb
+++ test/unit/exception_handling/sensu_test.rb
@@ -3,49 +3,48 @@
 require File.expand_path('../../test_helper',  __dir__)
 
 module ExceptionHandling
-  class SensuTest < ActiveSupport::TestCase
+  describe Sensu do
     context "#generate_event" do
-      should "create an event" do
-        mock(ExceptionHandling::Sensu).send_event(name: "world_is_ending", output: "stick head between knees and kiss ass goodbye", status: 1)
+      it "create an event" do
+        expect(ExceptionHandling::Sensu).to receive(:send_event).with(name: "world_is_ending", output: "stick head between knees and kiss ass goodbye", status: 1)
 
         ExceptionHandling::Sensu.generate_event("world_is_ending", "stick head between knees and kiss ass goodbye")
       end
 
-      should "add the sensu prefix" do
+      it "add the sensu prefix" do
         ExceptionHandling.sensu_prefix = "cnn_"
 
-        mock(ExceptionHandling::Sensu).send_event(name: "cnn_world_is_ending", output: "stick head between knees and kiss ass goodbye", status: 1)
+        expect(ExceptionHandling::Sensu).to receive(:send_event).with(name: "cnn_world_is_ending", output: "stick head between knees and kiss ass goodbye", status: 1)
 
         ExceptionHandling::Sensu.generate_event("world_is_ending", "stick head between knees and kiss ass goodbye")
       end
 
-      should "allow the level to be set to critical" do
-        mock(ExceptionHandling::Sensu).send_event(name: "world_is_ending", output: "stick head between knees and kiss ass goodbye", status: 2)
+      it "allow the level to be set to critical" do
+        expect(ExceptionHandling::Sensu).to receive(:send_event).with(name: "world_is_ending", output: "stick head between knees and kiss ass goodbye", status: 2)
 
         ExceptionHandling::Sensu.generate_event("world_is_ending", "stick head between knees and kiss ass goodbye", :critical)
       end
 
-      should "error if an invalid level is supplied" do
-        dont_allow(ExceptionHandling::Sensu).send_event
+      it "error if an invalid level is supplied" do
+        expect(ExceptionHandling::Sensu).to_not receive(:send_event)
 
-        assert_raise(RuntimeError, "Invalid alert level") do
+        expect do
           ExceptionHandling::Sensu.generate_event("world_is_ending", "stick head between knees and kiss ass goodbye", :hair_on_fire)
-        end
+        end.to raise_exception(RuntimeError, "Invalid alert level")
       end
     end
 
     context "#send_event" do
-      setup do
+      before do
         @event = { name: "world_is_ending", output: "stick head between knees and kiss ass goodbye", status: 1 }
         @socket = SocketStub.new
       end
 
-      should "send event json to sensu client" do
-        mock.any_instance_of(Addrinfo).connect.with_any_args { @socket }
-
+      it "send event json to sensu client" do
+        expect_any_instance_of(Addrinfo).to receive(:connect).with(any_args) { @socket }
         ExceptionHandling::Sensu.send_event(@event)
 
-        assert_equal @event.to_json, @socket.sent.first
+        expect(@socket.sent.first).to eq(@event.to_json)
       end
     end
   end
