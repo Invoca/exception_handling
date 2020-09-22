7a67165739d1c5e768bbf163bc23c34ee5be3f78
Yinon Rousso <yrousso@invoca.com>
2020-09-22 17:12:54 -0600

    TECH-4826 chage tests to use rspec in exception_catalog_test.rb

--- test/unit/exception_handling/exception_catalog_test.rb
+++ test/unit/exception_handling/exception_catalog_test.rb
@@ -3,73 +3,73 @@
 require File.expand_path('../../test_helper',  __dir__)
 
 module ExceptionHandling
-  class ExceptionCatalogTest < ActiveSupport::TestCase
+  describe ExceptionCatalog do
 
     context "With stubbed yaml content" do
-      setup do
+      before do
         filter_list = { exception1: { error: "my error message" },
                         exception2: { error: "some other message", session: "misc data" } }
-        stub(YAML).load_file { filter_list }
+        allow(YAML).to receive(:load_file) { filter_list }
 
         # bump modified time up to get the above filter loaded
-        stub(File).mtime { incrementing_mtime }
+        allow(File).to receive(:mtime) { incrementing_mtime }
       end
 
       context "with loaded data" do
-        setup do
-          stub(File).mtime { incrementing_mtime }
+        before do
+          allow(File).to receive(:mtime) { incrementing_mtime }
           @exception_catalog = ExceptionCatalog.new(ExceptionHandling.filter_list_filename)
           @exception_catalog.send :load_file
         end
 
-        should "have loaded filters" do
-          assert_equal 2, @exception_catalog.instance_eval("@filters").size
+        it "have loaded filters" do
+          expect(@exception_catalog.instance_eval("@filters").size).to eq(2)
         end
 
-        should "find messages in the catalog" do
-          assert !@exception_catalog.find(error: "Scott says unlikely to ever match")
+        it "find messages in the catalog" do
+          expect(!@exception_catalog.find(error: "Scott says unlikely to ever match")).to be_truthy
         end
 
-        should "find matching data" do
+        it "find matching data" do
           exception_description = @exception_catalog.find(error: "this is my error message, which should match something")
-          assert exception_description
-          assert_equal :exception1, exception_description.filter_name
+          expect(exception_description).to be_truthy
+          expect(exception_description.filter_name).to eq(:exception1)
         end
       end
 
-      should "write errors loading the yaml file directly to the log file" do
+      it "write errors loading the yaml file directly to the log file" do
         @exception_catalog = ExceptionCatalog.new(ExceptionHandling.filter_list_filename)
 
-        mock(ExceptionHandling).log_error.never
-        mock(ExceptionHandling).write_exception_to_log(anything, "ExceptionCatalog#refresh_filters: ./config/exception_filters.yml", anything)
-        mock(@exception_catalog).load_file { raise "noooooo" }
+        expect(ExceptionHandling).to receive(:log_error).never
+        expect(ExceptionHandling).to receive(:write_exception_to_log).with(anything, "ExceptionCatalog#refresh_filters: ./config/exception_filters.yml", anything)
+        expect(@exception_catalog).to receive(:load_file) { raise "noooooo" }
 
         @exception_catalog.find({})
       end
     end
 
     context "with live yaml content" do
-      setup do
+      before do
         @filename = File.expand_path('../../../config/exception_filters.yml',  __dir__)
         @exception_catalog = ExceptionCatalog.new(@filename)
-        assert_nothing_raised do
+        expect do
           @exception_catalog.send :load_file
-        end
+        end.not_to raise_error
       end
 
-      should "load the filter data" do
-        assert !@exception_catalog.find(error: "Scott says unlikely to ever match")
-        assert !@exception_catalog.find(error: "Scott says unlikely to ever match")
+      it "load the filter data" do
+        expect(!@exception_catalog.find(error: "Scott says unlikely to ever match")).to be_truthy
+        expect(!@exception_catalog.find(error: "Scott says unlikely to ever match")).to be_truthy
       end
     end
 
     context "with no yaml content" do
-      setup do
+      before do
         @exception_catalog = ExceptionCatalog.new(nil)
       end
 
-      should "not load filter data" do
-        mock(ExceptionHandling).write_exception_to_log.with_any_args.never
+      it "not load filter data" do
+        expect(ExceptionHandling).to receive(:write_exception_to_log).with(any_args).never
         @exception_catalog.find(error: "Scott says unlikely to ever match")
       end
     end
