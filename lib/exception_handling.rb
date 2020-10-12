3d7b0330d749940e03cf5995dbf181af7d15a3d2
Yinon Rousso <yrousso@invoca.com>
2020-10-12 08:53:54 -0700

    TECH-4326 rm newlines

--- lib/exception_handling.rb
+++ lib/exception_handling.rb
@@ -348,10 +348,7 @@ module ExceptionHandling # never included
       nil
     end
 
-    deprecate :escalate_to_production_support,
-      :escalate_error,
-      :escalate_warning,
-      :ensure_escalation,
+    deprecate :escalate_to_production_support, :escalate_error, :escalate_warning, :ensure_escalation,
       deprecator: ActiveSupport::Deprecation.new('3.0', 'ExceptionHandling')
 
     def alert_warning(exception_or_string, alert_name, exception_context, log_context)
